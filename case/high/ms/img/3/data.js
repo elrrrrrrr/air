8560744be4493fc916ba33e87d9245014b58e4eb
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-11 17:48:34 +0800

    add prizeDrao interface

---
+++ case/high/ms/img/3/data.js
@@ -0,0 +1,45 @@
+// pageTracker._trackPageview("events/market/ttzj/hangzhou");
+
+
+//0-美甲 1-美发 2-遇见 3-密室
+window.type = '3';
+window.banner='img/banner.jpg';
+window.storeList = [{
+        "name": "Pulupulu主题游戏馆",
+        "imgs": ["img/3/1-1.jpg","img/3/1-2.jpg"],
+        "promotion": "",
+        "coupon": "http://t.dianping.com/deal/2031990",
+        "link": "http://www.dianping.com/shop/13813428"
+    }, {
+        "name": "X先生密室(黄浦旗舰店)",
+        "imgs": ["img/3/2.jpg"],
+        "promotion": "http://www.dianping.com/promo/221204?source=1",
+        "coupon": "http://t.dianping.com/deal/166787",
+        "link": "http://www.dianping.com/shop/6028133"
+    }, {
+        "name": "Black Queen黑桃皇后密室逃脱",
+        "imgs": ["img/3/3.jpg"],
+        "promotion": "",
+        "coupon": "http://t.dianping.com/deal/2082681",
+        "link": "http://www.dianping.com/shop/15859249"
+    },
+    {
+        "name": "FBI真人密室",
+        "imgs": ["img/3/4.jpg"],
+        "promotion": "",
+        "coupon": "",
+        "link": "http://www.dianping.com/shop/18217327"
+    }, {
+        "name": "极道真人密室逃脱(人民广场大世界店)",
+        "imgs": ["img/3/5.jpg"],
+        "promotion": "",
+        "coupon": "",
+        "link": "http://www.dianping.com/shop/18179096"
+    }, {
+        "name": "睿趣Reach真人密室逃脱(四川北路店)",
+        "imgs": ["img/3/6.jpg"],
+        "promotion": "",
+        "coupon": "http://t.dianping.com/deal/2049465",
+        "link": "http://www.dianping.com/shop/8854466"
+    },
+];
